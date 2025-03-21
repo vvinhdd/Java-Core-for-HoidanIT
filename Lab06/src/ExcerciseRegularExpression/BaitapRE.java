d1ab4c583cba618b58b7f06bbb59875e7e612f2e
Duc Vinh <ddvinh13@gmail.com>
2025-03-21 15:52:01 +0700

    Complete Lab06

--- Lab06/src/ExcerciseRegularExpression/BaitapRE.java
+++ Lab06/src/ExcerciseRegularExpression/BaitapRE.java
@@ -9,13 +9,32 @@ public class BaitapRE {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		System.out.println("Nhap CCCD can kiem tra: ");
-		String check = s.nextLine();
 		
-		// System.out.println(check.length()); // 123456789102
-		//checkCCCD(check);
-		//checkPassword(check);
-		checkEmail(check);
+		System.out.println("1. Kiem tra CCCD: ");
+		System.out.println("2. Kiem tra Password: ");
+		System.out.println("3. Kiem tra Email: ");
+		System.out.println("Lua chon phuong thuc muon kiem tra: ");
+		String option = s.nextLine();
+		
+		switch(option) {
+			case "1":
+				System.out.println("Nhap CCCD can kiem tra: ");
+				String check1 = s.nextLine();
+				checkCCCD(check1);
+				break;
+			case "2":
+				System.out.println("Nhap Password can kiem tra: ");
+				String check2 = s.nextLine();
+				checkPassword(check2);
+				break;
+			case "3":
+				System.out.println("Nhap Email can kiem tra: ");
+				String check3 = s.nextLine();
+				checkEmail(check3);
+				break;
+			default:
+				System.out.println("Chua chon lua chon nao");
+		}
 		
 		s.close();
 	}
